a025f7e8a7e6ef0101537d498b3b3d2c2c2f7a89
Shahm <its.shahm@hotmail.com>
2026-06-08 21:19:21 +0300

    feat(articles): add insights card stagger animation CSS

--- fix_uuid.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "_blog_posts_v_version_internal_links_suggestions" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-ALTER TABLE "_blog_posts_v_version_references" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-ALTER TABLE "_blog_posts_v_version_external_sources" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-ALTER TABLE "_blog_posts_v_version_related_services" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-
-ALTER TABLE "blog_posts_internal_links_suggestions" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-ALTER TABLE "blog_posts_references" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-ALTER TABLE "blog_posts_external_sources" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
-ALTER TABLE "blog_posts_related_services" ADD COLUMN IF NOT EXISTS "_uuid" VARCHAR;
--- test-blog.ts
+++
@@ -1,7 +0,0 @@
-import { getPublishedBlogPosts } from './lib/blog/data'
-
-async function test() {
-  const data = await getPublishedBlogPosts('en')
-  console.log('Result length:', data.length)
-}
-test()
